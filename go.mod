77c89b731239db4114a07d9e91fda32c3b9751af
Le Hoang Nam <namlh5@vng.com.vn>
2022-12-29 21:39:30 +0700

    add simple lexer

---
+++ go.mod
@@ -0,0 +1,5 @@
+module monkey
+
+go 1.18
+
+require github.com/matryer/is v1.4.0 // indirect
---
+++ lexer/lexer_test.go
@@ -0,0 +1,87 @@
+package lexer
+
+import (
+	"testing"
+
+	"monkey/token"
+
+	"github.com/matryer/is"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `let five = 5;
+let	ten = 10;
+
+let add = fn(x, y) {
+	x + y;
+};
+
+let result = add(five, ten);
+
+!-/*5;
+5 < 10 > 5;
+`
+	testcases := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+	assert := is.New(t)
+	for _, tc := range testcases {
+		tok := l.NextToken()
+		assert.Equal(tc.expectedType, tok.Type)
+		assert.Equal(tc.expectedLiteral, tok.Literal)
+	}
+
+}
